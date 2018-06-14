1a74fd6ff457e8e3f92c238b04ddb7ed09e473ea
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-06-14 01:18:17 +0000

    Created initial Header, Footer and Home components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Components
+import Header from "./header";
+import Footer from "./footer";
+import Home from "./home";
 
 class App extends Component {
   render() {
-    return <div>Hello world</div>;
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
